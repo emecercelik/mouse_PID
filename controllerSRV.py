b609fc8c7558176d3484a0890e2ff4f349b178d3
emecercelik <emec.ercelik@gmail.com>
2015-07-21 11:17:33 +0200

    Update controllerSRV.py

--- controllerSRV.py
+++ controllerSRV.py
@@ -69,42 +69,43 @@ servo_ids["thigh.R"]       = setPositionServo(reference_object_name = "obj_thigh
 
 #np.random.seed(np.random.randint(0,10000))
 
-ax_avg=0.1
+ax_avg=0.1 #Global parameter to calculate avg acceleration
 ay_avg=0.1
 az_avg=0.1
 
-ax=.1
+ax=.1 #Global parameter to record acceleration
 ay=.1
 az=.1
 
 # PID Parameters
-nJoint=12
-kP=np.array([.1 for i in range(nJoint)])
+nJoint=12 #number of joints that is controlled
+kP=np.array([.1 for i in range(nJoint)]) #coefficients of PID controller
 kI=np.array([.03 for i in range(nJoint)])
 kD=np.array([.1 for i in range(nJoint)])
 
-e_old=np.array([0. for i in range(nJoint)])
-E=np.array([0. for i in range(nJoint)])
-uu=np.array([0. for i in range(nJoint)])
+e_old=np.array([0. for i in range(nJoint)]) #The error one step before
+E=np.array([0. for i in range(nJoint)]) #Integrated error
+uu=np.array([0. for i in range(nJoint)]) # Control input initialization
 
-act_tmp=.0
+inpp=[] # Record inputs of servos to be plotted
+outp=[] # Record outputs of servos to be plotted 
+cont_inp=[] # Reecord control inputs to servos to be plotted
 
-inpp=[]
-outp=[]
-cont_inp=[]
 # Joints
-
+#Joint names
 joints=["wrist.L","wrist.R","forearm.L","forearm.R","upper_arm.L","upper_arm.R",\
         "shin_lower.L","shin_lower.R","shin.L","shin.R","thigh.L","thigh.R"]
+numRec=3 # The number of joint to be plotted
 
-numRec=3
 # =================================================================================================================================================
 #                                       Evolve function
 def evolve():
+    # Global variable definitions
     global ax,ay,az,ax_avg,ay_avg,az_avg
-    global act_tmp,inpp,outp,PP
+    global inpp,outp,PP
     global kP,kI,kD,e_old,E,uu
     global joints,nJoint,numRec
+    
     print("Step:", i_bl, "  Time:{0:.2f}".format(t_bl),'   Acc:{0:8.2f}  {1:8.2f}  {2:8.2f}'.format(ax,ay,az),\
           'Acc:{0:8.2f} {1:8.2f} {2:8.2f}'.format(ax_avg,ay_avg,az_avg))
     # ------------------------------------- Visual ------------------------------------------------------------------------------------------------
@@ -124,19 +125,20 @@ def evolve():
     # ------------------------------------- Neural Simulation -------------------------------------------------------------------------------------
     
     
-    ax=vestibular_array[3]
+    ax=vestibular_array[3] # Get instant accelerations
     ay=vestibular_array[4]
     az=vestibular_array[5]
 
-    ax_avg=(ax_avg*(i_bl)+ax)/(i_bl+1)
+    ax_avg=(ax_avg*(i_bl)+ax)/(i_bl+1) # Calculate avg accelerations
     ay_avg=(ay_avg*(i_bl)+ay)/(i_bl+1)
     az_avg=(az_avg*(i_bl)+az)/(i_bl+1)
 
 
     # ------------------------------------- Muscle Activation -------------------------------------------------------------------------------------
     
-    speed_ = 6.0
+    speed_ = 6.0 # Speed of the mouse (ang. freq of joint patterns)
 
+    # Joint signals to be applied
     act_tmp         = 0.5 + 0.5*np.sin(speed_*t_bl)
     anti_act_tmp    = 1.0 - act_tmp
     act_tmp_p1      = 0.5 + 0.5*np.sin(speed_*t_bl - np.pi*0.5)
@@ -144,30 +146,33 @@ def evolve():
     act_tmp_p2      = 0.5 + 0.5*np.sin(speed_*t_bl + np.pi*0.5)
     anti_act_tmp_p2 = 1.0 - act_tmp_p2
 
+    # Reference value of joints
     r=np.array([0.4,0.4,0.8*act_tmp,0.8*anti_act_tmp,1.0*act_tmp_p1,1.0*anti_act_tmp_p1,0.8*anti_act_tmp,\
                 0.8*act_tmp,0.5*anti_act_tmp_p1,0.5*act_tmp_p1,0.5*anti_act_tmp,0.5*act_tmp])
 
     if i_bl<=0:
         for i in range(nJoint):
             controlActivity(control_id = servo_ids[joints[i]], control_activity = r[i])
+            # Apply the reference at the first step to the servos
     else:
         for i in range(nJoint):
             controlActivity(control_id = servo_ids[joints[i]], control_activity = uu[i])
+            # Apply the control inputs to the servos 
 
+    # Get actual positions of joints after the control input
     positions=np.array([getMuscleSpindle(control_id = servo_ids[joints[i]])[0] for i in range(len(joints))])
     
 
-    e=r-positions
-    E=E+e
-    uu=kP*e+kI*E+kD*(e-e_old)
-    e_old=e
+    e=r-positions # Calculateinstant errors for all joints
+    E=E+e # Integrate errors for all joints
+    uu=kP*e+kI*E+kD*(e-e_old) # Calculate control inputs for all joints
+    e_old=e # Replace old error
     
     
-    inpp.append(r[numRec])
-    outp.append(positions[numRec])
-    cont_inp.append(uu[numRec])
+    inpp.append(r[numRec]) # Record reference
+    outp.append(positions[numRec]) # record positions of servos
+    cont_inp.append(uu[numRec]) # Reecord control inputs
 
     if i_bl==750:
         data=np.hstack((np.array(inpp),np.array(outp),np.array(cont_inp),joints[numRec],PP,speed_))
-        PickleIt(data,'inpOut')
-
+        PickleIt(data,'inpOut') # Save the data to be plotted
